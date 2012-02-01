0e621cd0f05a731190d4bb87f43d0538f2cc0a62
Charlie Melbye <charlie@yourwiki.net>
2012-02-01 18:36:15 -0500

    navbar stuff login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+	helper_method :current_user
+	helper_method :logged_in?
+
+	protected
+	def current_user
+		@user ||= User.find(session[:id])
+	end
+
+	def logged_in?
+		!!current_user
+	end
 end
