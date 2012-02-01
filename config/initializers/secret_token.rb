bc92b2473916ef63f71465aa791272919aca90f1
Charlie Melbye <charlie@yourwiki.net>
2012-01-31 19:34:06 -0500

    import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Focus::Application.config.secret_token = '340bc7df1989b6bb30f1808ef6159b83a4bf65995715ccad99e5a9d90fe2e5bd46a107d8b3fcb9bf1cab74f9ccfe9b72b3c597a799ab0bb7c538c0f7d7d4a8c8'
