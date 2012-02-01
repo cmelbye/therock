f02bde1ed0c3c20019a2870c33374a960b25d572
Charlie Melbye <charlie@yourwiki.net>
2012-01-31 20:18:05 -0500

    uid

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 	def create
 		auth_hash = request.env["omniauth.auth"].to_hash
-		@user = User.find_or_create_by_id(auth_hash["uid"])
+		@user = User.find_or_create_by_uid(auth_hash["uid"])
 		@user.update_attributes(:name => auth_hash["info"]["name"], :first_name => auth_hash["info"]["first_name"], :last_name => auth_hash["info"]["last_name"], :icon => auth_hash["info"]["image"], :email => auth_hash["info"]["email"], :token => auth_hash["credentials"]["token"])
 		session[:user_id] = @user.id
 		render :text => @user.id
--- db/migrate/20120201005511_create_users.rb
+++ db/migrate/20120201005511_create_users.rb
@@ -1,6 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+			t.string :uid
       t.string :name
       t.string :email
       t.string :first_name
