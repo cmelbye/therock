503a7aa37132199c9979cb0839e30e0d33560330
Charlie Melbye <charles.melbye@gmail.com>
2012-02-02 12:38:49 -0700

    fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
 	has_many :documents, :through => :document_contributors, :uniq => true, :order => 'documents.updated_at DESC'
 
 	def name
-		first_name + " " + last_name
+		if first_name && last_name
+			first_name + " " + last_name
+		else
+			"(unnamed staff member)"
+		end
 	end
 end
