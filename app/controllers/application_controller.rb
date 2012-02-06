03cb20f4c90fd7345a002c4ba1f604af5d3ee75d
Charlie Melbye <charles.melbye@gmail.com>
2012-02-06 08:22:21 -0700

    better postinst and maintenance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
 	helper_method :current_user
 	helper_method :logged_in?
 
+	before_filter :show_maintenance_page
+
 	protected
 	def current_user
 		if session[:user_id]
@@ -11,6 +13,10 @@ class ApplicationController < ActionController::Base
 		else
 			@user = nil
 		end
+	end
+
+	def show_maintenance_page
+		render :template => "layouts/maintenance"
 	end
 
 	def logged_in?
