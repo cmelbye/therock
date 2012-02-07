b8d8a9d3d9a296f82382c5af8902869e12db7148
Charlie Melbye <charles.melbye@gmail.com>
2012-02-07 12:53:08 -0700

    actually fix

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
-redis_config = YAML.load(File.load(Rails.root + 'config/redis.yml'))[Rails.env]
+redis_config = YAML.load(File.open(Rails.root + 'config/redis.yml'))[Rails.env]
 
 if redis_config
 	REDIS = Redis.new(:host => redis_config['host'], :port => redis_config['port'], :password => redis_config['password'])
