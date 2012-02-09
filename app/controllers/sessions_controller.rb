94c9568e0b7379f228d0aa773f9f379aeb5d7428
Charlie Melbye <charles.melbye@gmail.com>
2012-02-09 01:38:46 -0700

    fix first/last name setting

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,16 @@ class SessionsController < ApplicationController
 	def create
 		auth_hash = request.env["omniauth.auth"].to_hash
 		@user = User.find_or_create_by_uid(auth_hash["uid"])
-		@user.update_attributes(:icon => auth_hash["info"]["image"], :email => auth_hash["info"]["email"], :token => auth_hash["credentials"]["token"])
+		attrs = {:icon => auth_hash["info"]["image"], :email => auth_hash["info"]["email"], :token => auth_hash["credentials"]["token"]}
+		
+		unless @user.first_name
+			attrs[:first_name] = auth_hash["info"]["first_name"]
+		end
+		unless @user.last_name
+			attrs[:last_name] = auth_hash["info"]["last_name"]
+		end
+
+		@user.update_attributes(attrs)
 		session[:user_id] = @user.id
 		redirect_to admin_path
 	end
